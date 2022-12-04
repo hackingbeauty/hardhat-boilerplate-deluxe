c70798a2249375940f515b3dbd5844b5d498a23a
Mark Muskardin <markmuskardin@gmail.com>
2022-12-03 16:34:20 -0800

    added hardhat-gas-report and dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,13 +4,20 @@ require("@nomicfoundation/hardhat-toolbox");
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
+require("hardhat-gas-reporter");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
     }
+  },
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
   }
 };
